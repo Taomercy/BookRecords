1d46c1fb2748cf1f041b834ceec6f63b2a981214
taomercy <taomercy@qq.com>
2019-07-10 23:22:51 -0700

    html template demo

--- BookRecords/settings.py
+++ BookRecords/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    ("css", os.path.join(STATIC_ROOT, 'datatables', 'css')),
+    ("img", os.path.join(STATIC_ROOT, 'datatables', 'images')),
+    ("js", os.path.join(STATIC_ROOT, 'datatables', 'js')),
+    ("fonts", os.path.join(STATIC_ROOT, 'datatables', 'fonts')),
+]
